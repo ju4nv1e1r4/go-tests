1f33129d6a172e61d83e56b2b40e44da76c7d1a6
juanvieira <juanvieira1@yahoo.com>
2025-02-03 14:55:22 -0300

    refactor test function

--- address/address_test.go
+++ address/address_test.go
@@ -1,17 +1,35 @@
 package address
 
-import "testing"
+import (
+	"testing"
+)
+
+type testScenarios struct {
+	addressInserted string
+	addressExpected string
+}
 
 // Unit test
 func TestAddressType(t *testing.T)  {
-	AddressForTest := "Street One"
-	addressExpected := "Street"
+	
+	data_receveid := []testScenarios {
+		{ "Avenue One", "Avenue" },
+		{ "Street One", "Street" },
+		{ "Road One", "Road" },
+		{ "Longview St", "Invalid Type" },
+		{ "", "Invalid Type" },
+		{ "STREET ZERO", "Street" },
+		{ "Ave Marvelous", "Invalid Type" },
+		{ "Highway 2H", "Highway" },
+	}
 
-	AddressReceived := AddressType(AddressForTest)
-	if AddressReceived != addressExpected {
-		t.Errorf("Unexpected type of address!\n Expected: %s \n Received: %s",
-		addressExpected,
-		AddressReceived,
-	)
+	for _, scenario := range data_receveid {
+		address_type := AddressType(scenario.addressInserted)
+		if address_type != scenario.addressExpected {
+			t.Errorf("Unexpected type of address!\n Expected: %s \n Received: %s",
+				address_type,
+				scenario.addressExpected,
+			)
+		}
 	}
 }
